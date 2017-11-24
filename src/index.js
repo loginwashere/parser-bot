c3c1cc1a003bece771675c3e258c1f79d9a0adfc
loginwashere <login.was.here@gmail.com>
2017-11-24 23:18:54 +0200

    Refactor, add parsing town news

--- src/index.js
+++ src/index.js
@@ -4,19 +4,17 @@ const schedule = require('node-schedule');
 const TelegramBot = require('node-telegram-bot-api');
 const mongoose = require('mongoose');
 const feed = require('feed-read');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
 const crypto = require('crypto');
 const debug = require('debug')('app');
 
 const superagent = require('superagent');
 require('superagent-charset')(superagent);
 
-const feedUrl = 'http://www.zhilstroj-2.ua/rss/';
-const gaskUrl = 'http://91.205.16.115/declarate/list.php?sort=num&order=DESC';
-
 const chatId = process.env.TELEGRAM_CHAT_ID;
 const rssItemsCollectionName = process.env.RSS_ITEMS_COLLECTION_NAME || 'rss_items';
 const documentsCollectionName = process.env.DOCUMENTS_COLLECTION_NAME || 'documents';
+const townNewsCollectionName = process.env.TOWN_NEWS_COLLECTION_NAME || 'town_news';
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 
@@ -59,7 +57,23 @@ documentSchema.virtual('title').get(function () { return `${this.id} ${this.obje
 
 const DocumentItem = mongoose.model('DocumentItem', documentSchema);
 
-const start = async () => {
+const townNewsSchema = mongoose.Schema({
+  id: String,
+  title: String,
+  number: String,
+  date: String,
+  session: String,
+  organizer: String,
+  kind: String,
+  original: String,
+  status: String,
+}, { collection: townNewsCollectionName });
+
+const TownNewsItem = mongoose.model('TownNewsItem', townNewsSchema);
+
+const processFeed = async () => {
+  const feedUrl = 'http://www.zhilstroj-2.ua/rss/';
+
   const res = await superagent
     .get(feedUrl)
     .charset('win1251');
@@ -101,6 +115,10 @@ const start = async () => {
       })
       .then(accumulator => debug(accumulator));
   });
+};
+
+const processGask = async () => {
+  const gaskUrl = 'http://91.205.16.115/declarate/list.php?sort=num&order=DESC';
 
   const fields = [
     'id',
@@ -151,7 +169,6 @@ const start = async () => {
         $(element)
           .children('td')
           .map((i, childElement) => {
-            $(childElement).text();
             item[fields[i]] = $(childElement).text().trim();
           });
         items.push(item)
@@ -178,13 +195,113 @@ const start = async () => {
       })
       .then(accumulator => debug(accumulator));
   }
+};
+
+const processTownNews = async () => {
+  const loginUrl = 'https://doc.citynet.kharkov.ua/ru/user/login';
+  const searchUrl = 'https://doc.citynet.kharkov.ua/ru/profile/search/index?doc_id_label=&doc_id=&doc_id_div=0&doc_id_end=&qstring=%D0%96%D0%B8%D1%82%D0%BB%D0%BE%D0%B1%D1%83%D0%B4-2&in_text=0&all_words=0&doc_number_label=&doc_number_pref=&doc_number_lb1=&doc_number_suff=&doc_number_div=0&doc_number_pref_end=&doc_number_lb2=&doc_number_suff_end=&doc_convocation_label=&doc_convocation=&doc_convocation_div=0&doc_convocation_end=&doc_session_label=&doc_session=&doc_session_div=0&doc_session_end=&doc_date_label=&doc_date_from=&doc_date_div=0&doc_date_end=&doc_dateadd_label=&doc_dateadd_from=&doc_dateadd_div=0&doc_dateadd_end=&newSearch=1&total=&cat=&organ=&status=&original=0&search=%D0%98%D1%81%D0%BA%D0%B0%D1%82%D1%8C';
+
+  const password = process.env.TOWN_NEWS_PASSWORD;
+
+  const hash = crypto.createHash('md5');
+  hash.update(password);
+
+  const hashString = hash.digest('hex');
+  debug(hashString);
+
+  const agent = superagent.agent();
+
+  await agent
+    .get(loginUrl);
+
+  await agent
+    .post(loginUrl)
+    .type('form')
+    .send({ login: process.env.TOWN_NEWS_LOGIN })
+    .send({ password })
+    .send({ hash_password: hashString });
+
+  const searchResponse = await agent
+    .get(searchUrl);
+
+  debug(searchResponse.text);
+
+  const $ = cheerio.load(searchResponse.text);
+
+  const fields = [
+    'title',
+    'number',
+    'date',
+    'session',
+    'organizer',
+    'kind',
+    'original',
+    'status',
+  ];
+
+  const items = [];
+  $('dt.check > input')
+    .map((index, element) => {
+      const item = {};
+      item.id = $(element).val().trim();
+      $(element)
+        .parent('dt')
+        .parent('dl')
+        .find('dd.s_r_item_wrapp dl')
+        .children()
+        .map((i, childElement) => {
+          item[fields[i]] = $(childElement).text().trim();
+        });
+      items.push(item)
+    });
+
+  debug(items);
+
+  all(items.map(townNewsItemAlreadyStored))
+    .then(accumulator => {
+      debug(accumulator);
+      return all(accumulator
+        .filter(item => item.status === 'resolved')
+        .map(item => item.value)
+        .filter(Boolean)
+        .map(storeTownNewsItem))  
+    })
+    .then(accumulator => {
+      debug(accumulator);
+      return all(accumulator
+        .filter(item => item.status === 'resolved')
+        .map(item => item.value)
+        .filter(Boolean)
+        .map(notify))
+    })
+    .then(accumulator => debug(accumulator));
+};
+
+const run = async () => {
+  try {
+    await processFeed();
+  } catch (e) {
+    debug('ERROR', e);
+  }
+
+  try {
+    await processGask();
+  } catch (e) {
+    debug('ERROR', e);
+  }
+
+  try {
+    await processTownNews();
+  } catch (e) {
+    debug('ERROR', e);
+  }
 }
 
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   const j = schedule.scheduleJob('* * * * *', () => {
-    start();
+    run();
   });
 });
 
@@ -218,6 +335,13 @@ function documentItemAlreadyStored(documentItem) {
       : Promise.resolve(documentItem))
 }
 
+function townNewsItemAlreadyStored(townNewsItem) {
+  return TownNewsItem.findOne({ id: townNewsItem.id })
+    .then(foundTownNewsItem => foundTownNewsItem
+      ? Promise.reject(TownNewsItem)
+      : Promise.resolve(townNewsItem))
+}
+
 function storeRssItem(rssItem) {
   const newRssItem = new RssItem(rssItem);
   return newRssItem.save();
@@ -226,6 +350,11 @@ function storeRssItem(rssItem) {
 function storeDocumentItem(documentItem) {
   const newDocumentItem = new DocumentItem(documentItem);
   return newDocumentItem.save();
+}
+
+function storeTownNewsItem(townNewsItem) {
+  const newTownNewsItem = new TownNewsItem(townNewsItem);
+  return newTownNewsItem.save();
 }
 
 function notify(item) {
